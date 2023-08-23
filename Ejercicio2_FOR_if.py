33222df53ed654e3e4ef789209aa24625578d84a
Octavio2023 <octavio.moya@istea.com.ar>
2023-08-23 19:50:29 -0300

    Create Ejercicio2_FOR_if.py

---
+++ Ejercicio2_FOR_if.py
@@ -0,0 +1,16 @@
+'''
+Realice un programa que enumere los paises de la siguiente
+lista:
+    paises = ["Canada", "USA", "Mexico", "Australia"]
+'''
+
+paises = ["Canada", "USA", "Mexico", "Australia"] #lista
+
+for pais in paises:
+    print(pais)
+    
+'''
+El for crea la variable temporal "pais" que es la que va a
+correr la lista de "paises". Entonces en cada repeticion
+la variable "pais" va a tener un diferente valor
+'''
